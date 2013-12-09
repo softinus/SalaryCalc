86becaea400827f9f30853a5f5e93132893481ca
thks10 <thks10@dd9611eb-c3ec-0410-9c58-dd6b21eb5c10>
2013-12-09 15:06:35 +0000

    -프로젝트 정리. -유니티 삽질중

---
+++ trunk/SalaryCalc_Unity/Assets/MainScript.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class MainScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+	{
+	
+	}
+
+
+
+	void OnGUI()
+	{
+		//if (!btnTexture)
+		//{
+			//Debug.LogError("Please assign a texture on the inspector");
+			//return;
+		//}
+		if (GUI.Button(new Rect(300, 380, 120, 50), "Start!"))
+			Debug.Log("Clicked the button with an image");
+		
+		//if (GUI.Button(new Rect(10, 70, 50, 30), "Click"))
+			//Debug.Log("Clicked the button with text");
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
