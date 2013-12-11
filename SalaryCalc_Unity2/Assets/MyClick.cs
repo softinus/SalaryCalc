b2762e0b51a9cf61334ff96646ddf91187fe8e0d
thks10 <softinus@users.noreply.github.com>
2013-12-11 15:28:14 +0000

    다시 만듦 +NGUI 까지

---
+++ SalaryCalc_Unity2/Assets/MyClick.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MyClick : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void MyClick2()
+	{
+		Debug.Log("click start!");
+		Application.LoadLevel("main2");
+	}
+}
