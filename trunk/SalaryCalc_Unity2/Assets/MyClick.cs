6f6c07f8403f20b0a965d209b226830494cd29b0
thks10 <thks10@dd9611eb-c3ec-0410-9c58-dd6b21eb5c10>
2013-12-12 15:01:27 +0000

    -3번째 씬 추가 -애니메이션 추가중.

--- trunk/SalaryCalc_Unity2/Assets/MyClick.cs
+++ trunk/SalaryCalc_Unity2/Assets/MyClick.cs
@@ -13,9 +13,21 @@ public class MyClick : MonoBehaviour {
 	
 	}
 
-	public void MyClick2()
+	public void MyClick_start()
 	{
 		Debug.Log("click start!");
 		Application.LoadLevel("main2");
+	}
+
+	public void MyClick_yes()
+	{
+		Debug.Log("click yes!");
+		Application.LoadLevel("main2");
+	}
+
+	public void MyClick_no()
+	{
+		Debug.Log("click no!");
+		//Application.LoadLevel("main2");
 	}
 }
